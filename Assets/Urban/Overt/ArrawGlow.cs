d73fc2a7d44fd3db24e2ecbe4432bcfafefc880c
vincent <vviinncceenntt21@gmail.com>
2023-11-14 21:09:15 -0700

    Update Arraw

---
+++ Assets/Urban/Overt/ArrawGlow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrawGlow : MonoBehaviour
+{
+    //public Material Mat;
+    public AnimationCurve ScaleCarve;
+    //public AnimationCurve TransparentCarve;
+    Vector3 OriginalScale = Vector3.one;
+    private void OnEnable()
+    {
+        OriginalScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        transform.localScale = ScaleCarve.Evaluate(Time.time % 1) * OriginalScale;
+        //transform.localScale = ScaleCarve.Evaluate(Time.time % 1) * OriginalScale;
+    }
+}
